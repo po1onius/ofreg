7f0c7752440e21b8330a95d53a1b7008072dda83
po1onius <po1onius@outlook.com>
2025-05-30 21:45:51 +0800

    add clap parse arg

--- ofreg/src/main.rs
+++ ofreg/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use ofreg_common::{OfregData, SOCK_PATH, TABLE_NAME};
 use tabled::Table;
 use tokio::{
@@ -5,19 +6,46 @@ use tokio::{
     net::UnixStream,
 };
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// query command
+    #[arg(short, long, conflicts_with = "file")]
+    cmd: Option<String>,
+
+    /// query file
+    #[arg(short, long, conflicts_with = "cmd")]
+    file: Option<String>,
+
+    /// query line number
+    #[arg(short, default_value_t = 10)]
+    n: u32,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+
+    let mut select = format!("SELECT * FROM {} LIMIT 10", TABLE_NAME);
+    if let Some(cmd) = args.cmd {
+        select = format!(
+            "SELECT op_file, time FROM {} WHERE cmd = '{}' LIMIT 10",
+            TABLE_NAME, cmd
+        );
+    }
+
+    if let Some(file) = args.file {
+        select = format!(
+            "SELECT cmd, time FROM {} WHERE op_file = '{}' LIMIT 10",
+            TABLE_NAME, file
+        );
+    }
+
     let mut stream = UnixStream::connect(SOCK_PATH).await.unwrap();
-
-    let select = format!("SELECT * FROM {} LIMIT 10", TABLE_NAME);
-    let cmd = "systemd-timesyncd";
-    let select_cmd = format!(
-        "SELECT * FROM {} WHERE cmd = '{}' LIMIT 10",
-        TABLE_NAME, cmd
-    );
-
-    stream.write_u32(select_cmd.len() as u32).await.unwrap();
-    stream.write_all(select_cmd.as_bytes()).await.unwrap();
+    println!("{select}");
+    stream.write_u32(select.len() as u32).await.unwrap();
+    stream.write_all(select.as_bytes()).await.unwrap();
 
     let mut ofreg_data_vec = Vec::new();
 
